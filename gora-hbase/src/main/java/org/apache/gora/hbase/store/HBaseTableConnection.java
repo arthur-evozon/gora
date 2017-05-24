a440117d83077b2e2b6ae133654b18a829f6e9c0
Arthur Bidian <arthur.bidian@evozon.com>
2017-05-24 14:11:46 +0300

    WIP: closing

--- gora-hbase/src/main/java/org/apache/gora/hbase/store/HBaseTableConnection.java
+++ gora-hbase/src/main/java/org/apache/gora/hbase/store/HBaseTableConnection.java
@@ -120,9 +120,6 @@ public class HBaseTableConnection {
     // (As an extra safeguard one might employ a shared variable i.e. 'closed'
     //  in order to prevent further table creation but for now we assume that
     //  once close() is called, clients are no longer using it).
-
-    LOG.info("\n\n\t>>> closing connection\n");
-
     flushCommits();
 
     for (Table table : tPool) {
@@ -130,6 +127,9 @@ public class HBaseTableConnection {
 
       LOG.info("\n\n\t>>> closing connection to '{}'...\n",table);
     }
+
+    LOG.info("\n\n\t>>> closing connection\n");
+    connection.close();
   }
 
   public Configuration getConfiguration() {
